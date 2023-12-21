bfa893e09b628697dd342245c1bc1aebb93516e1
saurabh-r1 <sidlock2@gmail.com>
2023-12-21 08:07:48 +0530

    doing two actions

--- redux-demo.js
+++ redux-demo.js
@@ -1,11 +1,19 @@
 const redux = require ('redux'); // importing redux
 
 const counterReducer = (state = {counter:0}, action) => {
-    
+    if (action.type==='increment'){
         return {
             counter: state.counter + 1,
         }
+    }
     
+    if (action.type ==='decrement'){
+        return{
+            counter:state.counter - 1,
+        }
+    }
+
+    return state;
     
 };
 
@@ -19,3 +27,9 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch ({type:'increment'});
+store.dispatch ({type:'increment'});
+store.dispatch ({type:'increment'});
+store.dispatch ({type:'increment'});
+store.dispatch ({type:'increment'});
+
+store.dispatch({type:'decrement'});
